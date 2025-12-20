80b23c5b5dd1c585b3a4e4c7c4574af6c06b1324
Martin Popov <me@martinpopov.com>
2025-12-20 13:20:40 +0200

    Added basic support for shadcn with base-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 
-import Test from "@/Test";
+import { Button } from "@/components/ui/button";
 
 function App() {
     const [count, setCount] = useState(0);
 
+    const onClick = () => {
+        setCount((prev) => prev + 1);
+    };
+
     return (
-        <>
-            <Test />
-        </>
+        <main>
+            <Button onClick={onClick}>Click me</Button>
+            <p>Count: {count}</p>
+        </main>
     );
 }
 
--- src/Test.tsx
+++
@@ -1,5 +0,0 @@
-const Test = () => {
-    return <div className="test">Test Components</div>;
-};
-
-export default Test;
